1ab0a33e1c9324f4bf858839adbce45c01883869
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2023-07-12 02:27:16 +0200

    WebGL update

--- Assets/Prefab/GameEntities/GameSignal/GameSignal.cs
+++ Assets/Prefab/GameEntities/GameSignal/GameSignal.cs
@@ -1,24 +1,30 @@
+using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameSignal : MonoBehaviour {
 
-    [SerializeField] private GameObject light;
-    [SerializeField] private float millisecondWaitTime = 1000f;
+    [SerializeField] private AnimationClip flickering;
+    [SerializeField] private AnimationClip offLight;
     private bool _signalStarted = false;
 
+    
     public void StartSignal() {
         if (_signalStarted) return;
-        RecursiveLight();
+        //RecursiveLight();
+        gameObject.GetComponent<Animation>().clip = flickering;
+        gameObject.GetComponent<Animation>().Play();
+
         _signalStarted = true;
     }
 
     public void StopSignal() {
-        light.gameObject.SetActive(false);
+        gameObject.GetComponent<Animation>().clip = offLight;
+        gameObject.GetComponent<Animation>().Play();
         _signalStarted = false;
     }
 
-    async private void RecursiveLight() {
+    /*async private void RecursiveLight() {
         while(true) {
 
             light.gameObject.SetActive(true);
@@ -27,7 +33,8 @@ public class GameSignal : MonoBehaviour {
             light.gameObject.SetActive(false);
             await Task.Delay((int)(millisecondWaitTime / 2f));
 
+            Debug.LogWarning("A Debug.Warning() call");
             if(!_signalStarted) return;
         }
-    }
+    }*/
 }
