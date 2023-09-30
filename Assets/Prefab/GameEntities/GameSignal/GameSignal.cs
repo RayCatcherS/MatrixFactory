2ca998de77e0ce2910a4cf959bd8cba9cc2f7bca
Stefano <romanelli1996@gmail.com>
2023-09-30 16:24:11 +0200

    bomb anim fix

--- Assets/Prefab/GameEntities/GameSignal/GameSignal.cs
+++ Assets/Prefab/GameEntities/GameSignal/GameSignal.cs
@@ -3,31 +3,14 @@ using UnityEngine;
 
 public class GameSignal : MonoBehaviour {
 
-    [SerializeField] private GameObject light;
-    [SerializeField] private float millisecondWaitTime = 1000f;
     private bool _signalStarted = false;
 
     public void StartSignal() {
-        if (_signalStarted) return;
-        RecursiveLight();
-        _signalStarted = true;
+        gameObject.GetComponent<Animation>().enabled = true;
     }
 
     public void StopSignal() {
-        light.gameObject.SetActive(false);
-        _signalStarted = false;
+        gameObject.GetComponent<Animation>().enabled = false;
     }
 
-    async private void RecursiveLight() {
-        while(true) {
-
-            light.gameObject.SetActive(true);
-            await Task.Delay((int)(millisecondWaitTime / 2f));
-
-            light.gameObject.SetActive(false);
-            await Task.Delay((int)(millisecondWaitTime / 2f));
-
-            if(!_signalStarted) return;
-        }
-    }
 }
